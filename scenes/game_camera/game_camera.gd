2a5060ba30b23b349f1d1b4a1ba282e81b514141
Firebelley <firebelley@users.noreply.github.com>
2023-04-29 09:14:30 -0400

    initial player work

---
+++ scenes/game_camera/game_camera.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var shaky_camera: Camera2D = $ShakyCamera2D
+
+var target_position: Vector2 = Vector2.ZERO
+
+
+func _ready():
+	shaky_camera.make_current()
+
+
+func _process(delta):
+	var player = get_tree().get_first_node_in_group("player")
+	
+	if player != null:
+		target_position = player.global_position;
+	
+#	shaky_camera.
+	global_position = global_position.lerp(target_position, 1.0 - exp(-16 * delta))
+#	shaky_camera.global_position = global_position.round()
+#	global_position = global_position.round()
+	shaky_camera.global_position = global_position.round()
---
+++ scenes/player/player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 110
+const ACCEL_SMOOTH = -30.0
+const GRAVITY = 600
+const JUMP_FORCE = 300
+const JUMP_UP_GRAVITY_MOD = .5
+const JUMP_TERMINATION_MOD = 4.0
+
+@onready var visuals: Node2D = $Visuals
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	var movement_vector = get_movement_vector()
+	velocity.x = lerp(velocity.x, movement_vector.x * MAX_SPEED, 1.0 - exp(ACCEL_SMOOTH * delta))
+	velocity.y += GRAVITY * delta
+	
+	if movement_vector.y < 0:
+		velocity.y -= JUMP_FORCE
+	
+	if velocity.y < 0 && Input.is_action_pressed("jump"):
+		var adjusted_gravity = GRAVITY * JUMP_UP_GRAVITY_MOD
+		velocity.y += adjusted_gravity * delta
+	elif velocity.y < 0 && !Input.is_action_pressed("jump"):
+		velocity.y += GRAVITY * JUMP_TERMINATION_MOD * delta
+	else:
+		velocity.y += GRAVITY * delta
+	
+	move_and_slide()
+	
+	visuals.global_position = global_position.round()
+	update_facing()
+
+func get_movement_vector():
+	var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y = -1 if Input.is_action_just_pressed("jump") && is_on_floor() else 0
+
+	return Vector2(x, y)
+
+
+func update_facing():
+	visuals.scale = Vector2(-1, 1) if get_movement_vector().x < 0 else Vector2.ONE
